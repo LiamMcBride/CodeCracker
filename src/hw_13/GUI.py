13db6d1ceeb25bb54fa49ccfa64b0e941e139196
LiamMcBride <mailmcbride56@gmail.com>
2021-11-30 14:32:42 -0500

    added front end ui bits

--- src/hw_13/GUI.py
+++ src/hw_13/GUI.py
@@ -1,16 +1,18 @@
-from tkinter import Tk, Label, Button
+from tkinter import *
 
 class GUI:
     def __init__(self, master):
         self.fontType = "Ariel"
         self.minWidth = 1000
         self.minHeight = 600
-        self.gray = "#202124"
+        self.bgColor = "#17223B"
+        self.fgColor = "#263859"
+        self.textColor = "#FF6768"
         self.master = master
         master.title("Code Cracker")
         #master.geometry(self.screenSize)
         master.minsize(self.minWidth, self.minHeight)
-        master.configure(background = self.gray)
+        master.configure(background = self.bgColor)
         #master.attributes('-fullscreen', True)
 
         self.label = Label(
@@ -18,17 +20,85 @@ class GUI:
             text="Welcome to Code Cracker",
             height = 2,
             font = (self.fontType, 25),
-            bg = self.gray,
-            fg = "white"
+            bg = self.bgColor,
+            fg = self.textColor
             )
+        #.grid(column=4)
         self.label.pack()
 
+        self.label2 = Label(
+            master,
+            text="Please enter desired password:",
+            height = 2,
+            font = (self.fontType, 15),
+            bg = self.bgColor,
+            fg = self.textColor
+            )
+        self.label2.pack()
 
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        self.input = Entry(
+            master,
+            font = (self.fontType, 15),
+            text="password",
+        )
+        self.input.pack()
 
-        self.close_button = Button(master, text="close", command = master.quit)
+        self.spacer = Label(
+            master,            
+            height = 2,
+            bg = self.bgColor,
+            )
+        self.spacer.pack()
+
+        self.label2 = Label(
+            master,
+            text="Choose SHA Encoding:",
+            height = 2,
+            font = (self.fontType, 15),
+            bg = self.bgColor,
+            fg = self.textColor
+            )
+        self.label2.pack()
+
+        v = StringVar(master, "1")
+
+        choices = {"SHA 5" : "1",
+                    "SHA 2" : "2"}
+
+        for (text, value) in choices.items():
+            Radiobutton(
+                master,
+                text=text,
+                variable = v,
+                value = value,
+                indicator = 0,
+                command = None,
+                bg = self.bgColor,
+                fg = self.textColor,
+                font = (self.fontType, 10)
+            ).pack()
+
+        self.spacer = Label(
+            master,            
+            height = 10,
+            bg = self.bgColor,
+            )
+        self.spacer.pack()
+
+        self.close_button = Button(
+            master,
+            text="Quit",
+            command=master.quit,
+            font = (self.fontType, 10),
+            bg = self.bgColor,
+            fg = self.textColor
+            )
         self.close_button.pack()
+
+
+
+    def shaValue(self, value):
+        print(value)
 
     def greet(self):
         print("Greetings!")
